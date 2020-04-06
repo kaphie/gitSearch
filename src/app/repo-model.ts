4ba22d10a64c9151d090102dacf076664bfe5df1
kaphie <bkafrika144@gmail.com>
2020-04-06 13:02:31 +0300

    adding content to class repo-model

--- src/app/repo-model.ts
+++ src/app/repo-model.ts
@@ -2,7 +2,7 @@ export class RepoModel {
   description: any;
   name: any;
   owner: any;
-  watchers: any;
+  watchers: any
   forks: any;
   // tslint:disable-next-line:variable-name
   default_branch: any;
