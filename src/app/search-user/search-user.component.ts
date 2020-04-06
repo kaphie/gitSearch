c4f30358f049584728f9b13074a4d274f26ddc21
kaphie <bkafrika144@gmail.com>
2020-04-06 12:57:15 +0300

    adding ts content to  component search-user

--- src/app/search-user/search-user.component.ts
+++ src/app/search-user/search-user.component.ts
@@ -6,8 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-user.component.css']
 })
 export class SearchUserComponent implements OnInit {
+  searchTerm: string;
+  @Output() searchEmmiter = new EventEmitter<any>();
 
-  constructor() { }
+  constructor() {
+   }
+
+
+  emmitUser() {
+    this.searchEmmiter.emit(this.searchTerm);
+    }
+
 
   ngOnInit(): void {
   }
